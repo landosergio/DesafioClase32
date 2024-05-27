4fcf01b297bc5fa6fed1d397f15cbc0e09aa9565
landosergio <lando.sergio@gmail.com>
2024-05-27 12:32:06 -0300

    handleErrors
    Terminado en products.

--- src/services/errors/info.js
+++ src/services/errors/info.js
@@ -1,12 +1,12 @@
 export function generateProductErrorInfo(product) {
   return `One or more properties were incomplete or not valid.
     List of required properties:
-    *title : needs to be a String, received **${product.title}**
-    *description : needs to be a String, received **${product.description}**
-    *price : needs to be a Number, received **${product.price}**
-    *thumbnail : needs to be an Array, received **${JSON.stringify(
-      product.thumbnail
-    )}**
-    *code : needs to be a String, received **${product.code}**
-    *stock : needs to be a Number, received **${product.stock}**`;
+    *title : needs to be a String, received **${typeof product.title}**
+    *description : needs to be a String, received **${typeof product.description}**
+    *price : needs to be a Number, received **${typeof product.price}**
+    *thumbnail : needs to be an Array, received **${
+      Array.isArray(product.thumbnail) ? "array" : typeof product.thumbnail
+    }**
+    *code : needs to be a String, received **${typeof product.code}**
+    *stock : needs to be a Number, received **${typeof product.stock}**`;
 }
